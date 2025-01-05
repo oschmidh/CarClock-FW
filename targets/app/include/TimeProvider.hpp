d4f81e6ed9aedb7898d04f769b15d8d60b77c379
oschmidh <74605541+oschmidh@users.noreply.github.com>
2025-01-05 10:34:23 +0100

    HomeScreen: Display time and icons

--- targets/app/include/TimeProvider.hpp
+++ targets/app/include/TimeProvider.hpp
@@ -1,7 +1,11 @@
 #ifndef CARINFOTAINMENTSYSTEM_FW_APP_INCLUDE_TIMEPROVIDER_H
 #define CARINFOTAINMENTSYSTEM_FW_APP_INCLUDE_TIMEPROVIDER_H
 
-struct Time { };
+struct Time {
+    unsigned int hours{};
+    unsigned int minutes{};
+    unsigned int seconds{};
+};
 
 class TimeProvider {
   public:
